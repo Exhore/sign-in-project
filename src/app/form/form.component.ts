565fdf53c564ee763b4698a4ff0e43061e053c6c
Exhore <nelsonrivca@gmail.com>
2024-02-05 10:45:58 +0000

    icon mat fix

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,16 +1,14 @@
 import { Component } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { Validators } from '@angular/forms';
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
 
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
-  styleUrls: ['./form.component.css']
+  styleUrls: ['./form.component.scss']
 })
+
+
 export class FormComponent {
   hide = true;
   emailFormControl = new FormControl('', [Validators.required, Validators.email]);
