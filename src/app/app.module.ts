e32a24012cfe03f32f98cf8188f9127c8dc12fe1
Exhore <nelsonrivca@gmail.com>
2024-02-08 21:03:31 +0000

    done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,36 +1,50 @@
+// imports the necessary modules for animations, forms, and material components
 import { Input, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { MatButtonModule } from '@angular/material/button';
 
-import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatInputModule } from '@angular/material/input';
 
 import { MatIconModule } from '@angular/material/icon';
 import { MatFormFieldModule } from '@angular/material/form-field';
 
-import { LandingPageComponent } from './landing-page/landing-page.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import {MatSliderModule} from '@angular/material/slider';
-
+import { MatSliderModule } from '@angular/material/slider';
 import { MatCardModule } from '@angular/material/card';
+
+// components
+import { AppComponent } from './app.component';
 import { FormComponent } from './form/form.component';
 import { BlueTrialComponent } from './blue-trial/blue-trial.component';
 import { PinkTrialComponent } from './pink-trial/pink-trial.component';
-import { AppRoutingModule } from './app-routing.module';
-import { SegundaPaginaComponent } from './segunda-pagina/segunda-pagina.component';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { SecondPageComponent } from './second-page/second-page.component';
+import { infoTitleComponent } from './infoTitle/infoTitle.component';
+import { HomePageComponent } from './home-page/home-page.component';
+
+
+
+// routes
+const routes: Routes = [
+  { path: '', component: HomePageComponent },
+  { path: 'GifSearch', component: SecondPageComponent }
+];
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    LandingPageComponent,
     FormComponent,
+    infoTitleComponent,
     BlueTrialComponent,
     PinkTrialComponent,
-    SegundaPaginaComponent
+    SecondPageComponent,
+    HomePageComponent
   ],
   imports: [
+    CommonModule,
     BrowserModule,
     BrowserAnimationsModule,
     MatButtonModule,
@@ -41,8 +55,7 @@ import { SegundaPaginaComponent } from './segunda-pagina/segunda-pagina.componen
     ReactiveFormsModule,
     MatCardModule,
     MatSliderModule,
-    AppRoutingModule
-
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/infoTitle/infoTitle.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'info-title',
+  templateUrl: './infoTitle.component.html',
+  styleUrls: ['./infoTitle.component.scss']
+})
+export class infoTitleComponent implements OnInit {
+
+  ngOnInit(): void {
+    // Initialization logic goes here
+  }
+
+}
